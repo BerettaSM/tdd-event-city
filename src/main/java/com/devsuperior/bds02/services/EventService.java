56924fd8f64ede1ab6fd6d3cd88a50cec351ee64
Ramon Saviato <ramonsaviato@hotmail.com>
2025-09-04 17:14:26 -0300

    test 'updateShouldReturnNotFoundWhenIdDoesNotExist' ok

--- src/main/java/com/devsuperior/bds02/services/EventService.java
+++ src/main/java/com/devsuperior/bds02/services/EventService.java
@@ -8,6 +8,9 @@ import com.devsuperior.bds02.dto.EventDTO;
 import com.devsuperior.bds02.entities.City;
 import com.devsuperior.bds02.entities.Event;
 import com.devsuperior.bds02.repositories.EventRepository;
+import com.devsuperior.bds02.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class EventService {
@@ -18,9 +21,14 @@ public class EventService {
     @Transactional
     public EventDTO update(Long id, EventDTO dto) {
         Event event = eventRepository.getReferenceById(id);
-        copyDtoToEntity(dto, event);
-        event = eventRepository.save(event);
-        return new EventDTO(event);
+        try {
+            copyDtoToEntity(dto, event);
+            event = eventRepository.save(event);
+            return new EventDTO(event);
+        }
+        catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException();
+        }
     }
 
     private void copyDtoToEntity(EventDTO dto, Event entity) {
